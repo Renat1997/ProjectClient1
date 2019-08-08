83e76e3150d680bd5c80303cbe3f7e6e581b38c1
Renat <renatgaynullin@yandex.ru>
2019-08-08 22:21:48 +0300

    1
    1

---
+++ ProjectClient/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace ProjectClient
+{
+    public partial class Form1 : Form
+    {
+       
+        public delegate void StartToStart(int port);
+        public event StartToStart Start;
+
+        public delegate void Ststop();
+        public event Ststop Stop;
+
+        public delegate void SendState(string state);
+        public event SendState Send;
+        
+       
+        public Form1()
+        {
+            Listener listener= new Listener(this);
+           
+            InitializeComponent();
+        }
+
+        public int GetPort
+        {
+            get
+            {
+                int result = 0;
+                int.TryParse(textBox1.Text, out result);
+                return result;
+            }
+        }
+        private void check() //проверка нажатия на checkbox
+        {
+            int power = checkBox1.Checked ? 1 : 0;
+            int cycle = checkBox2.Checked ? 1 : 0;
+            int crash = checkBox3.Checked ? 1 : 0;
+            Send?.Invoke(String.Concat(power, cycle, crash)); 
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            check();
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            check();
+        }
+
+        private void checkBox3_CheckedChanged(object sender, EventArgs e)
+        {
+            check();
+        }
+
+        
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Start?.Invoke(GetPort);
+        }
+        
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Stop?.Invoke();
+        }
+
+    }
+}
---
+++ ProjectClient/Listener.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace ProjectClient
+{
+    class Listener
+    {
+        public Listener(Form1 form)
+        {
+            form.Start += Start;
+
+            form.Stop += Stop;
+
+            form.Send += showMes;
+        }
+
+
+        private TcpClient client;
+        private CancellationTokenSource tokenSource;
+
+
+
+        //создание, ожидание клиента
+        private void StartListener(int port)
+        {
+
+            Task.Run(() =>
+            {
+                TcpListener server = null;
+                try
+                {
+                    int portInt = 0;
+                    try
+                    {
+                        portInt = Convert.ToInt32(port); // конвертация из стринг в инт
+
+                    }
+                    catch (Exception ex)
+
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+
+                    IPAddress adr = IPAddress.Parse("127.0.0.1");
+
+                    server = new TcpListener(adr, portInt);
+
+                    server.Start();
+
+                    while (!tokenSource.Token.IsCancellationRequested)
+                    {
+                        if (client == null || !client.Connected)
+                            client = server.AcceptTcpClient();
+
+                        Thread.Sleep(1);
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.Write(e);
+                }
+                finally
+                {
+                    server.Stop();
+                }
+
+            });
+
+
+        }
+
+
+        //проверка состояния
+        private void showMes(string state)
+        {
+            if (!tokenSource.Token.IsCancellationRequested)
+            {
+                try
+                {
+                    if (client != null && client.Connected == true)
+                    {
+                        NetworkStream stream = client.GetStream();
+                        byte[] msg = System.Text.Encoding.Unicode.GetBytes(state);
+                        stream.Write(msg, 0, msg.Length);
+                    }
+                }
+                catch
+                {
+
+                }
+            }
+        }
+
+        //старт листенера
+        private void Start(int port)
+        {
+            tokenSource = new CancellationTokenSource();
+            StartListener(port);
+        }
+
+        //стоп потоков
+        private void Stop()
+        {
+
+            tokenSource.Cancel();
+        }
+    }
+}
+
